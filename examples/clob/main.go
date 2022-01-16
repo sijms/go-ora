3d48e650a9185cc43bc2f452948c7983ca817b73
Samy Sultan <samyibraheem@gmail.com>
2022-01-16 23:16:28 +0200

    update clob example

--- examples/clob/main.go
+++ examples/clob/main.go
@@ -5,9 +5,14 @@ import (
 	"flag"
 	"fmt"
 	_ "github.com/sijms/go-ora/v2"
+	go_ora "github.com/sijms/go-ora/v2"
 	"io/ioutil"
 	"os"
 	"time"
+)
+
+var (
+	server string
 )
 
 func createTable(conn *sql.DB) error {
@@ -15,6 +20,7 @@ func createTable(conn *sql.DB) error {
 	sqlText := `CREATE TABLE GOORA_TEMP_VISIT(
 	VISIT_ID	number(10)	NOT NULL,
 	VISIT_DATA  CLOB,
+	VISIT_DATA2 CLOB,
 	PRIMARY KEY(VISIT_ID)
 	)`
 	_, err := conn.Exec(sqlText)
@@ -33,14 +39,71 @@ func dropTable(conn *sql.DB) error {
 	fmt.Println("Finish drop table: ", time.Now().Sub(t))
 	return nil
 }
-
+func readData(conn *sql.DB) error {
+	t := time.Now()
+	row := conn.QueryRow("SELECT VISIT_ID, VISIT_DATA, VISIT_DATA2 FROM GOORA_TEMP_VISIT")
+	if row != nil {
+		var (
+			visitID int64
+			data1   string
+			data2   string
+		)
+		err := row.Scan(&visitID, &data1, &data2)
+		if err != nil {
+			return err
+		}
+		fmt.Println("ID: ", visitID, "\t Data1: ", data1, "\t Data2: ", data2)
+		//fmt.Println("ID: ", visitID, "\t Data2: ", data2)
+	}
+	fmt.Println("1 row readed: ", time.Now().Sub(t))
+	return nil
+}
+func insertData2() error {
+	t := time.Now()
+	conn, err := go_ora.NewConnection(server)
+	if err != nil {
+		return err
+	}
+	err = conn.Open()
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			fmt.Println("Can't close connection2: ", err)
+		}
+	}()
+	if err != nil {
+		return err
+	}
+	val, err := ioutil.ReadFile("clob.json")
+	if err != nil {
+		return err
+	}
+	val2 := "string2"
+	stmt := go_ora.NewStmt(`INSERT INTO GOORA_TEMP_VISIT(VISIT_ID, VISIT_DATA, VISIT_DATA2) VALUES(2, :1, :2)`, conn)
+	defer func() {
+		err = stmt.Close()
+		if err != nil {
+			fmt.Println("Can't close stmt: ", err)
+		}
+	}()
+	stmt.AddParam(":1", string(val), len(val), go_ora.Input)
+	stmt.AddParam(":2", val2, len(val2), go_ora.Input)
+	_, err = stmt.Exec(nil)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Finish second insert: ", time.Now().Sub(t))
+	return nil
+}
 func insertData(conn *sql.DB) error {
 	t := time.Now()
 	val, err := ioutil.ReadFile("clob.json")
 	if err != nil {
 		return err
 	}
-	_, err = conn.Exec(`INSERT INTO GOORA_TEMP_VISIT(VISIT_ID, VISIT_DATA) VALUES(1, :1)`, val)
+
+	_, err = conn.Exec(`INSERT INTO GOORA_TEMP_VISIT(VISIT_ID, VISIT_DATA, VISIT_DATA2) VALUES(1, :1, :2)`,
+		string(val), "string2")
 	if err != nil {
 		return err
 	}
@@ -61,9 +124,6 @@ func usage() {
 	fmt.Println()
 }
 func main() {
-	var (
-		server string
-	)
 
 	flag.StringVar(&server, "server", "", "Server's URL, oracle://user:pass@server/service_name")
 	flag.Parse()
@@ -107,5 +167,15 @@ func main() {
 	err = insertData(conn)
 	if err != nil {
 		fmt.Println("Can't insert data: ", err)
+		return
+	}
+	err = insertData2()
+	if err != nil {
+		fmt.Println("Can't make second insert: ", err)
+		return
+	}
+	err = readData(conn)
+	if err != nil {
+		fmt.Println("Can't read data: ", err)
 	}
 }

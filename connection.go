1fee5601f68a6daa441c9b01cc8e559da31da4da
Samy Sultan <samyibraheem@gmail.com>
2022-08-03 01:18:16 +0300

    update v1 to connect to oracle 9

--- connection.go
+++ connection.go
@@ -219,15 +219,18 @@ func (conn *Connection) Prepare(query string) (driver.Stmt, error) {
 }
 
 func (conn *Connection) Ping(ctx context.Context) error {
-	conn.connOption.Tracer.Print("Ping")
-	conn.session.ResetBuffer()
-	conn.session.StartContext(ctx)
-	defer conn.session.EndContext()
-	return (&simpleObject{
-		connection:  conn,
-		operationID: 0x93,
-		data:        nil,
-	}).write().read()
+	if conn.dBVersion.Number >= 10102 {
+		conn.connOption.Tracer.Print("Ping")
+		conn.session.ResetBuffer()
+		conn.session.StartContext(ctx)
+		defer conn.session.EndContext()
+		return (&simpleObject{
+			connection:  conn,
+			operationID: 0x93,
+			data:        nil,
+		}).write().read()
+	}
+	return nil
 }
 
 //func (conn *Connection) Logoff() error {

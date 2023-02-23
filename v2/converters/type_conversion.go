1e964e9e618d4759d384ca4088d0c43ffed35b96
nova <alex.s.novikov@gmail.com>
2023-02-23 17:40:53 +0200

    Fix date and timestamp select

--- v2/converters/type_conversion.go
+++ v2/converters/type_conversion.go
@@ -564,7 +564,7 @@ func DecodeDate(data []byte) (time.Time, error) {
 	}
 	if tzHour == 0 && tzMin == 0 {
 		return time.Date(year, time.Month(data[2]), int(data[3]),
-			int(data[4]-1), int(data[5]-1), int(data[6]-1), nanoSec, time.UTC), nil
+			int(data[4]-1), int(data[5]-1), int(data[6]-1), nanoSec, time.Local), nil
 	}
 	var zone *time.Location
 	//var err error
@@ -620,8 +620,9 @@ func addDigitToMantissa(mantissaIn uint64, d byte) (mantissaOut uint64, carryOut
 // FromNumber decode Oracle binary representation of numbers
 // and returns mantissa, negative and exponent
 // Some documentation:
-//	https://gotodba.com/2015/03/24/how-are-numbers-saved-in-oracle/
-//  https://www.orafaq.com/wiki/Number
+//
+//		https://gotodba.com/2015/03/24/how-are-numbers-saved-in-oracle/
+//	 https://www.orafaq.com/wiki/Number
 func FromNumber(inputData []byte) (mantissa uint64, negative bool, exponent int, mantissaDigits int, err error) {
 	if len(inputData) == 0 {
 		return 0, false, 0, 0, fmt.Errorf("Invalid NUMBER")
